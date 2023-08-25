f6aef57bb9d4b4512589012f5319ab6a74add2d7
Jaume Amores <jaume.dsdev@gmail.com>
2023-08-25 09:15:40 +0100

    First version of first goal in the roadmap

--- nbmodular/core/cell2func.py
+++ nbmodular/core/cell2func.py
@@ -4,13 +4,14 @@
 __all__ = ['Cell2Func', 'load_ipython_extension', 'keep_variables']
 
 # %% ../../nbs/cell2func.ipynb 2
+from functools import reduce
 from pathlib import Path
 import sys
 import ast
 from IPython import get_ipython
 from IPython.core.magic import (Magics, magics_class, line_magic,
                                 cell_magic, line_cell_magic)
-
+from sklearn.utils import Bunch
 from fastcore.all import argnames
 
 # %% ../../nbs/cell2func.ipynb 3
@@ -21,8 +22,8 @@ class Cell2Func(Magics):
     """
     def __init__(self, shell):
         super().__init__(shell)
-        self.variable_values = {}
-        self.variables = {}
+        self.function_info = Bunch()
+        self.function_list = []
         
     @cell_magic
     def cell2file (self, folder, cell):
@@ -39,19 +40,52 @@ class Cell2Func(Magics):
     def function (self, func, cell):
         "Converts cell to function"
         
-        get_variables_before = f'\nkeep_variables ("before_{func}", locals ())'
-        get_ipython().run_cell(get_variables_before)
+        # parameters (temporary)
+        collect_variables_values = True
+        make_function = True
+        tab_size=4
         
-        cell += f'\nkeep_variables ("{func}", locals ())'
-        get_ipython().run_cell(cell)
+        this_function = Bunch(idx=len(self.function_list))
+        if func not in self.function_info:
+            self.function_info[func] = this_function
+            self.function_list.append (this_function)
+            
+        idx = this_function['idx']
         
-        self.variable_values[func] = {k:self.variable_values[func][k] for k in set(self.variable_values[func]).difference(self.variable_values[f'before_{func}'])}
-        
-        print (self.variable_values[func])
+        # get variables specific about this function
+        if collect_variables_values:
+            get_variables_before_code = f'\nkeep_variables ("{func}", "values_before", locals ())'
+            get_ipython().run_cell(get_variables_before_code)
+            
+            get_variables_here_code = cell + f'\nkeep_variables ("{func}", "values_here", locals ())'
+            get_ipython().run_cell(get_variables_here_code)
+            values_before, values_here = this_function['values_before'], this_function['values_here']
+            values_here = {k:values_here[k] for k in set(values_here).difference(values_before)}
+            this_function['values_here'] = values_here
+            print (values_here)
         
         root = ast.parse (cell)
-        self.variables[func] = sorted ({node.id for node in ast.walk(root) if isinstance(node, ast.Name) and not callable(eval(node.id))})
-        print (self.variables[func])
+        variables_here = {node.id for node in ast.walk(root) if isinstance(node, ast.Name) and not callable(eval(node.id))}
+        print (variables_here)
+        if idx > 0:
+            variables_before = reduce (lambda x, y: x['variables_here'] | y['variables_here'], self.function_list[:idx])
+        else:
+            variables_before = []
+        variables_here = sorted (variables_here.difference(variables_before))
+        print (variables_here)
+        this_function.update (variables_here=variables_here, variables_before=variables_before)
+        
+        if make_function:
+            function_code = ''
+            arguments = ', '.join (variables_before)
+            return_values = ','.join (variables_here + variables_before)
+            for line in cell.splitlines():
+                function_code += f'{" " * tab_size}{line}\n'
+            return_line = f'return {return_values}'
+            function_code = f'def {func}({arguments}):\n' + function_code + f'{" " * tab_size}{return_line}\n'
+            this_function['code'] = function_code
+            get_ipython().run_cell(function_code)
+            print (function_code)
 
 # %% ../../nbs/cell2func.ipynb 4
 def load_ipython_extension(ipython):
@@ -62,7 +96,7 @@ def load_ipython_extension(ipython):
     ipython.register_magics(magics)
 
 # %% ../../nbs/cell2func.ipynb 5
-def keep_variables (function, variables, self=None):
+def keep_variables (function, field, variable_values, self=None):
     """
     Store `variables` in dictionary entry `self.variables_field[function]`
     """
@@ -73,6 +107,6 @@ def keep_variables (function, variables, self=None):
         if len(args)>0:
             self = fr.f_locals[args[0]]
         frame_number += 1
-    variables = {k: variables[k] for k in variables if not k.startswith ('_') and not callable(variables[k])}
-    variables_field = getattr(self, 'variable_values')
-    variables_field[function] = variables
+    variable_values = {k: variable_values[k] for k in variable_values if not k.startswith ('_') and not callable(variable_values[k])}
+    function_info = getattr(self, 'function_info')
+    function_info[function][field]=variable_values
